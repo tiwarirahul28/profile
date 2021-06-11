80c2016472f8ba7c7a12dbe9fdce9677b74b5805
Rajan Tiwari <rajan@leadschool.in>
2021-06-11 20:53:43 +0530

    initial

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -3,6 +3,7 @@ import img2 from '../img/portImages/marvelsh.png';
 import img3 from '../img/portImages/covide.png';
 import img4 from '../img/portImages/fithit.png';
 import img5 from '../img/portImages/resort-1.png';
+import img6 from '../img/portImages/tesla-clone.png';
 
 const portfolios = [
     {
@@ -19,47 +20,46 @@ const portfolios = [
         category: 'Javascript',
         image: img2,
         link1: 'https://github.com/TheRahulTiwari28/marvel',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
+        // link2: 'https://www.google.com',
+        title: 'Marvel SH',
+        text: 'Collection Of SuperHeros & Their Movies'
     },
     {
         id: 3,
         category: 'Javascript',
         image: img3,
         link1: 'https://github.com/TheRahulTiwari28/covide-19',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
+        // link2: 'https://www.google.com',
+        title: 'Covide-19',
+        text: 'Symptoms & Precautions of covid 19'
     },
     {
         id: 4,
         category: 'Javascript',
         image: img4,
         link1: 'https://github.com/TheRahulTiwari28/fitness',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
+        // link2: 'https://www.google.com',
+        title: 'Fitness',
+        text: 'Fit India Hit India'
     },
     {
         id: 5,
         category: 'React JS',
         image: img5,
         link1: 'https://github.com/TheRahulTiwari28/resort',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
+        // link2: 'https://www.google.com',
+        title: 'Resort',
+        text: 'Resort is the ideal getaway for anyone who loves the sea.'
     },
-    // {
-    //     id: 6,
-    //     category: 'Bootstrap/Saas   ',
-    //     image: img2,
-    //     link1: 'https://www.google.com',
-    //     link2: 'https://www.google.com',
-    //     title: 'How To Use Blender',
-    //     text: 'Free Animation Software'
-    // },
-    
+    {
+        id: 6,
+        category: 'React JS',
+        image: img6,
+        link1: 'https://github.com/TheRahulTiwari28/tesla-clone',
+        // link2: 'https://www.google.com',
+        title: 'Tesla',
+        text: 'Tesla-Clone'
+    }
 ]
 
 export default portfolios;
